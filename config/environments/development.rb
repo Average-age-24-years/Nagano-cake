3acb9ccb15f06f2e8fadd95039793ae7402e0f56
turituri08 <t.y/bsk@icloud.com>
2021-06-23 14:15:24 +0900

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,8 @@ Rails.application.configure do
     port:                 587,
     address:              'smtp.gmail.com',
     domain:               'gmail.com',
-    user_name:            '',
-    password:             '',
+    user_name:            'a',
+    password:             'a',
     authentication:       'login',
     enable_starttls_auto: true
   }
