324b1c467b3880068fb539082d5402daefdd3ac7
terakawaakari <terakawaakari0701@gmail.com>
2021-06-23 06:32:17 +0000

    デザイン調整

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,30 +1,34 @@
 class Public::CartProductsController < ApplicationController
 
-  before_action :authenticate_customer!
+  before_action :authenticate_customer!, except: [:create]
 
   def index
     @cart_products = current_customer.cart_products.all
   end
 
   def create
-    @cart_product = current_customer.cart_products.new(cart_product_params)
-    @cart_products = current_customer.cart_products.all
-
-    unless @cart_product.quantity.present?
-  		redirect_to request.referrer, alert: "個数を選択してください"
+    if customer_signed_in?
+      @cart_product = current_customer.cart_products.new(cart_product_params)
+      @cart_products = current_customer.cart_products.all
+  
+      unless @cart_product.quantity.present?
+    		redirect_to request.referrer, alert: "個数を選択してください"
+      else
+        @cart_products.each do |cart_product|
+    	    if cart_product.product_id == @cart_product.product_id
+    			  sum_of_quantity = cart_product.quantity + @cart_product.quantity
+    			  cart_product.update_attribute(:quantity, sum_of_quantity)
+    			  redirect_to public_cart_products_path, notice: "カートに商品を追加しました"
+    			  @cart_product.delete
+    	    end
+  		  end
+      end
+    	if @cart_product.save
+    		redirect_to public_cart_products_path, notice: "カートに商品を追加しました"
+    	end
     else
-      @cart_products.each do |cart_product|
-  	    if cart_product.product_id == @cart_product.product_id
-  			  sum_of_quantity = cart_product.quantity + @cart_product.quantity
-  			  cart_product.update_attribute(:quantity, sum_of_quantity)
-  			  redirect_to public_cart_products_path, notice: "カートに商品を追加しました"
-  			  @cart_product.delete
-  	    end
-		  end
+  	  redirect_to new_customer_session_path
     end
-  	if @cart_product.save
-  		redirect_to public_cart_products_path, notice: "カートに商品を追加しました"
-  	end
   end
 
   def update
