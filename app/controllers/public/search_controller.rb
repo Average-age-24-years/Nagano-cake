0f487f4ef77db5a98332b79cbdb9ea67aada0a92
taikinakano <kabayaki3323@gmail.com>
2021-06-27 05:26:21 +0000

    ジャンル検索機能削除

--- app/controllers/public/search_controller.rb
+++ app/controllers/public/search_controller.rb
@@ -12,15 +12,6 @@ class Public::SearchController < ApplicationController
               record = @content
            end
            end
-		if @model == 'genre'
-			@records = Genre.search_for(record, @method).page(params[:page]).reverse_order
-            @record = Product.find(product_params)
-			@active_products = Product.where(is_active: true)
-		else
 			@records = Product.search_for(record, @method).page(params[:page]).reverse_order
-		end
 	end
-  def product_params
-    params.require(:product).permit(:name, :image_id, :introduction, :price, :name, :conversion_name)
-  end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,7 +1,9 @@
 class Genre < ApplicationRecord
 
   has_many :products
-
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, format: {with: VALID_EMAIL_REGEX}, allow_blank: true
   validates :name,  presence: true
   
   def self.search_for(content, method)
